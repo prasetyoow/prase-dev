80dd0b81bfa08f4dae61d34c7e02236b2332d899
prasetyoow <praztamer@gmail.com>
2023-02-21 20:45:15 +0700

    added constant data

---
+++ src/constants.js
@@ -0,0 +1,89 @@
+export const projects = [
+  {
+    title: "Dicurre React Web",
+    description:
+      "A website for transferring money to anyone you connected. The website is made using React.js as a framework and CSS/Bootstrap for the styling, Redux as state management.",
+    image: "/images/project-1.png",
+    tags: ["React", "JavaScript"],
+    source: "https://github.com/prasetyoow/dicurre-reactjs",
+    visit: "https://dicurre-reactjs.vercel.app/",
+    id: 0,
+  },
+  {
+    title: "Dicurre React Native",
+    description:
+      "A mobile application for transferring money to anyone you connected. The application is made using React Native as a framework for mobile platform (Android and IOS) and StyleSheet for the styling and all the styles are converted to variable.",
+    image: "/images/project-2.png",
+    tags: ["React Native", "JavaScript"],
+    source: "https://github.com/prasetyoow/dicurre-react-native",
+    id: 1,
+  },
+  {
+    title: "Dicurre Backend (eWallet Project)",
+    description:
+      "A backend program for Dicurre website and application, using Express and Node Js for the framework and PostgreSQL for the database, CDN for media management. An e-wallet website and application to transfer money to anyone you connected.",
+    image: "/images/project-3.png",
+    tags: ["Express", "Node"],
+    source: "https://github.com/prasetyoow/dicurre-backend",
+    visit: "https://dicurre.vercel.app/",
+    id: 2,
+  },
+  {
+    title: "Cuaca (Weather App)",
+    description:
+      "A website for instantly access forecasts weather in a city around the world, using OpenWeather API.",
+    image: "/images/project-4.png",
+    tags: ["React", "JavaScript"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 3,
+  },
+  {
+    title: "Slicing personal web",
+    description: "A personal website slicing from Figma design.",
+    image: "/images/project-5.png",
+    tags: ["NextJS", "JavaScript"],
+    source: "https://github.com/prasetyoow/personal-web",
+    visit: "https://prasetyoow.vercel.app/",
+    id: 4,
+  },
+  {
+    title: "Streamu Movie App",
+    description: "A movie app to find and search based on TMDB API.",
+    image: "/images/project-6.png",
+    tags: ["React", "JavaScript"],
+    source: "https://github.com/prasetyoow/Streamu-movie-app",
+    visit: "https://streamu-movie-app.vercel.app/",
+    id: 5,
+  },
+  {
+    title: "PokiDeck",
+    description:
+      "A pokemon dex to show and search a pokemon based on Pokedex API",
+    image: "/images/project-7.png",
+    tags: ["React", "JavaScript"],
+    source: "https://github.com/prasetyoow/PokiDeck",
+    visit: "https://poki-deck.vercel.app/",
+    id: 6,
+  },
+  {
+    title: "GPT3 Slicing",
+    description: "A modern UI slicing website based on Figma design",
+    image: "/images/project-8.png",
+    tags: ["React", "CSS"],
+    source: "https://github.com/prasetyoow/GPT3_LandingPage",
+    visit: "https://openai-gpt3-henna.vercel.app/",
+    id: 7,
+  },
+  {
+    title: "React Projects",
+    description: "A compilation of react projects using basic or fundamental",
+    image: "/images/project-9.png",
+    tags: ["React", "JavaScript"],
+    source: "https://github.com/prasetyoow/react-projects",
+    visit: "https://react-projects-prasetyoow.vercel.app/",
+    id: 8,
+  },
+];
+
+export default projects;
